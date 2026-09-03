5a470ee6b65af52062f0cc30393f7561cebeec80
sooryabot09 <soorya7thej9@gmail.com>
2026-09-03 22:07:40 +0530

    Replace static screenshot with video preview for Portfolio V2 project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@ import duckImg from "../assets/projects/duck2.jpg";
 import guessmeImg from "../assets/projects/guessme.jpg";
 import snakeImg from "../assets/projects/snake.jpg";
 import pf1Img from "../assets/projects/pf1.jpg";
-import pf2Img from "../assets/projects/pf2.png";
+import pf2Video from "../assets/projects/pf2.mp4";
 
 const PROJECTS = [
   {
@@ -76,7 +76,7 @@ const PROJECTS = [
     link2: null,
     color: "#0b0f19",
     accent: "#38bdf8",
-    media: pf2Img,
+    media: pf2Video,
   },
 ];
 
